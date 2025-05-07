196af730a49e0909ece3d89ddcef1b2fd329bbb0
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-07 08:01:36 -0400

    created bolt11 decode function

---
+++ src/functions/sendPayment.js
@@ -0,0 +1,29 @@
+import { decode } from "light-bolt11-decoder";
+import { sparkPaymenWrapper } from "./payments";
+
+export async function decodeLNPayment(address) {
+  try {
+    const decoded = decode(address.toLowerCase());
+
+    const feeResponse = await sparkPaymenWrapper({
+      getFee: true,
+      address: decoded.paymentRequest,
+      paymentType: "lightning",
+      amountSats:
+        decoded.sections.find((item) => item.name === "amount").value / 1000,
+    });
+
+    if (!feeResponse.didWork) throw new Error(feeResponse.error);
+
+    return {
+      invoice: decoded.paymentRequest,
+      amount: decoded.sections.find((item) => item.name === "amount").value,
+      description: decoded.sections.find((item) => item.name === "description")
+        .value,
+      fee: feeResponse.fee,
+      paymentType: "lightning",
+    };
+  } catch (error) {
+    console.log("bolt11 decode error", error);
+  }
+}
