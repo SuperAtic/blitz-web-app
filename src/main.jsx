409c819a5242f8c874ff07ca62fd500214e1ed8c
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-07 07:31:11 -0400

    adding routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 
 import SafeAreaComponent from "./components/safeAreaContainer.jsx";
 import CreateSeed from "./pages/createSeed/createSeed.jsx";
@@ -17,91 +23,122 @@ import WalletHome from "./pages/wallet/wallet.jsx";
 import EditReceivePaymentInformation from "./pages/receiveAmount/receiveAmount.jsx";
 import ReceiveQRPage from "./pages/receiveQRPage/receiveQRPage.jsx";
 import Camera from "./pages/camera/camera.jsx";
+import SwitchReceiveOption from "./pages/switchReceiveOption/switchReceiveOption.jsx";
+import { AnimatePresence } from "framer-motion";
+import SendPage from "./pages/sendPage/sendPage.jsx";
+import ConfirmPayment from "./pages/confirmPayment/confirmPaymentScreen.jsx";
 
 function Root() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <AuthProvider navigate={navigate}>
       <SparkProvier navigate={navigate}>
         <AuthGate />
-        <Routes>
-          {/* Public Routes */}
-          <Route
-            path="/"
-            element={
-              <SafeAreaComponent>
-                <Home />
-              </SafeAreaComponent>
-            }
-          />
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            {/* Public Routes */}
+            <Route
+              path="/"
+              element={
+                <SafeAreaComponent>
+                  <Home />
+                </SafeAreaComponent>
+              }
+            />
 
-          <Route
-            path="/disclaimer"
-            element={
-              <SafeAreaComponent>
-                <DisclaimerPage />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/createAccount"
-            element={
-              <SafeAreaComponent>
-                <CreateSeed />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/createPassword"
-            element={
-              <SafeAreaComponent>
-                <CreatePassword />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <SafeAreaComponent>
-                <Login />
-              </SafeAreaComponent>
-            }
-          />
+            <Route
+              path="/disclaimer"
+              element={
+                <SafeAreaComponent>
+                  <DisclaimerPage />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/createAccount"
+              element={
+                <SafeAreaComponent>
+                  <CreateSeed />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/createPassword"
+              element={
+                <SafeAreaComponent>
+                  <CreatePassword />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <SafeAreaComponent>
+                  <Login />
+                </SafeAreaComponent>
+              }
+            />
 
-          <Route
-            path="/wallet"
-            element={
-              <SafeAreaComponent>
-                <WalletHome />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/receiveAmount"
-            element={
-              <SafeAreaComponent>
-                <EditReceivePaymentInformation />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/receive"
-            element={
-              <SafeAreaComponent>
-                <ReceiveQRPage />
-              </SafeAreaComponent>
-            }
-          />
-          <Route
-            path="/camera"
-            element={
-              <SafeAreaComponent>
-                <Camera />
-              </SafeAreaComponent>
-            }
-          />
-        </Routes>
+            <Route
+              path="/wallet"
+              element={
+                <SafeAreaComponent>
+                  <WalletHome />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/receiveAmount"
+              element={
+                <SafeAreaComponent>
+                  <EditReceivePaymentInformation />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/receive"
+              element={
+                <SafeAreaComponent>
+                  <ReceiveQRPage />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/send"
+              element={
+                <SafeAreaComponent>
+                  <SendPage />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/receive-options"
+              element={
+                <SafeAreaComponent>
+                  <SwitchReceiveOption />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/camera"
+              element={
+                <SafeAreaComponent>
+                  <Camera />
+                </SafeAreaComponent>
+              }
+            />
+            <Route
+              path="/confirm-page"
+              element={
+                <SafeAreaComponent>
+                  <ConfirmPayment />
+                </SafeAreaComponent>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </SparkProvier>
     </AuthProvider>
   );
