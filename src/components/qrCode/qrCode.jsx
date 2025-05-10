d9c94b6b9dfa0836621d7f7632312e3102be6fc9
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-10 15:26:51 -0400

    improved logo on image

--- src/components/qrCode/qrCode.jsx
+++ src/components/qrCode/qrCode.jsx
@@ -4,17 +4,14 @@ import "./style.css";
 export default function QRCodeQrapper({ data }) {
   return (
     <div className="qrContainer">
+      <div className="imageContainer">
+        <img src={logo} />
+      </div>
       <QRCodeSVG
         height={"100%"}
         width={"100%"}
         bgColor="transparent"
         value={data}
-        imageSettings={{
-          src: logo,
-          width: 20,
-          height: 20,
-          excavate: true,
-        }}
       />
     </div>
   );
