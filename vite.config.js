bebda324c6babccc57169a12b0754b08a0e77def
Blake Kaufman <blakekaufman9@gmail.com>
2025-05-17 11:50:13 -0400

    bump version

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     "process.env": {},
-    // "navigator.product": JSON.stringify("Web"), // pretend we're not in React Native
-  },
-  resolve: {
-    alias: {
-      "react-native": path.resolve(__dirname, "empty-react-native.js"),
-    },
   },
 });
