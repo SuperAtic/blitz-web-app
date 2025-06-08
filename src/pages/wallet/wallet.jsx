3a524d9eabcf58c2906da5eab2ec8ab411a06201
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-08 18:35:46 -0400

    fixed wallet padding bug

--- src/pages/wallet/wallet.jsx
+++ src/pages/wallet/wallet.jsx
@@ -1,16 +1,16 @@
 import TransactionContanier from "../../components/transactionContainer/transactionContianer";
-import { useSpark } from "../../contexts/sparkContext";
 import UserBalance from "./components/balanceContainer/userBalanceContainer";
 import WalletNavBar from "./components/nav/nav";
 import SendAndRequestBtns from "./components/sendAndRequestBTNS/sendAndRequstBtns";
+import "./wallet.css";
 
 export default function WalletHome() {
   return (
-    <>
+    <div id="walletHomeContainer">
       <WalletNavBar />
       <UserBalance />
       <SendAndRequestBtns />
       <TransactionContanier frompage={"home"} />
-    </>
+    </div>
   );
 }
