3e302e27a960911c4f2aa13d75dd2fc320f010b1
Blake Kaufman <blakekaufman9@gmail.com>
2025-06-14 10:55:27 -0400

    updated balance to inclue fiat

--- src/pages/wallet/components/balanceContainer/userBalanceContainer.jsx
+++ src/pages/wallet/components/balanceContainer/userBalanceContainer.jsx
@@ -1,12 +1,35 @@
+import FormattedSatText from "../../../../components/formattedSatText/formattedSatText";
+import { useGlobalContextProvider } from "../../../../contexts/masterInfoObject";
 import { useSpark } from "../../../../contexts/sparkContext";
 import "./style.css";
 
 export default function UserBalance() {
   const { sparkInformation } = useSpark();
+  const { toggleMasterInfoObject, masterInfoObject } =
+    useGlobalContextProvider();
+
+  console.log(masterInfoObject);
   return (
     <div className="userBalanceContainer">
       <p>Total balance</p>
-      <h1>{sparkInformation.balance} sats</h1>
+      <div
+        onClick={() => {
+          toggleMasterInfoObject({
+            userBalanceDenomination:
+              masterInfoObject.userBalanceDenomination === "sats"
+                ? "fiat"
+                : masterInfoObject.userBalanceDenomination === "fiat"
+                ? "hidden"
+                : "sats",
+          });
+        }}
+        className="balanceContianer"
+      >
+        <FormattedSatText
+          styles={{ fontSize: "2rem" }}
+          balance={sparkInformation.balance}
+        />
+      </div>
     </div>
   );
 }
